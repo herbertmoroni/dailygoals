f59f2816cb925c292d8a7848de1d5508c7a9ab5b
herbertmoroni <herbertmoroni@hotmail.com>
2024-12-18 18:52:11 -0700

    updating vite

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,11 @@ export default defineConfig({
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
-  },
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        progress: resolve(__dirname, 'src/progress.html')
+      }
+    }
+  }
 });
