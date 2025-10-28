0d4c75972f8ac8bf9a2b14be5a6e1bad3e1b9682
herbertmoroni <herbertmoroni@hotmail.com>
2025-10-28 11:21:29 -0600

    Add publicDir configuration to Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
+  publicDir: 'public',
   build: {
     outDir: resolve(__dirname, 'dist'),
     emptyOutDir: true,
